1e65a14fe4e1e2bef41b2e259dc3ded14b1e39ba
DESKTOP-DG2UJNT\HPofDada <data02@gmail.com>
2023-03-15 14:16:57 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            
+
+            Console.WriteLine("Type 1 if you like to mingle and chat with people, or 2 if you're the opposite: ");
+            string input = Console.ReadLine();
+            Person myPerson = new Person();
+            myPerson.AskQuestion(int.Parse(input));
+            Console.WriteLine("You are: " + myPerson.GetPersonality());
+            }
+            catch
+            {
+                Console.WriteLine("Please type numbers 1 or 2");
+            }
+        }
+    }
+}

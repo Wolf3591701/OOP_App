d884b4724c319c35303e0c251a602de3d974e5c2
DESKTOP-DG2UJNT\HPofDada <data02@gmail.com>
2023-03-15 14:25:08 +0100

    Revert "Revert "Add project files.""
    This reverts commit a3a3172db70592aabff61ec4b365217331a33f0a.

---
+++ Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_App
+{
+    internal class Person
+    {
+        //Declare a field to represent personality
+        private string personality;
+
+        // Constructor
+        public Person()
+        {
+            personality = "Mystery";
+        }
+
+        public void AskQuestion(int answer)
+        {
+            if (answer == 1) 
+            {
+                personality = "Extrovert";
+            }
+            else
+            {
+                personality = "Introvert";
+            }
+        }
+
+        //This METHOD returns the value of "personality"
+        public string GetPersonality()
+        {
+            return personality;
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_App
+{
+    //Here we can only read or write data to this class
+    internal class Student : Person
+    {
+        //Private field (backing field) to hold any data assigned Name property.
+        private string _name;
+
+        private Student() 
+        {
+            _name = "";
+        }
+
+        //this is the Name property
+        public string Name
+        {
+            get { return _name; }  //use Get accessor to read data from the class
+            set { _name = value; } //Set accessor value is set automatically 
+        }
+
+    }
+}

4a10cd6855c543529d57394e089ecf9ec392f51e
DESKTOP-DG2UJNT\HPofDada <data02@gmail.com>
2023-03-15 21:52:21 +0100

    Added Student functionality, using OOP principles where possible and using Lists to store students info.

--- Program.cs
+++ Program.cs
@@ -8,20 +8,50 @@ namespace OOP_App
 {
     internal abstract class Program
     {
-        static void Main(string[] args)
-        {
-            try
+        //static void Main(string[] args)
+        //{
+        //    try
+        //    {
+        //        Console.WriteLine("Type 1 if you like to mingle and chat with people, or 2 if you're the opposite: ");
+        //        string input = Console.ReadLine();
+        //        Person myPerson = new Person();
+        //        myPerson.AskQuestion(int.Parse(input));
+        //        Console.WriteLine("You are: " + myPerson.GetPersonality());
+        //    }
+        //    catch
+        //    {
+        //        Console.WriteLine("Please type numbers 1 or 2");
+        //    }
+
+            class ProgramMain : Student
             {
-            Console.WriteLine("Type 1 if you like to mingle and chat with people, or 2 if you're the opposite: ");
-            string input = Console.ReadLine();
-            Person myPerson = new Person();
-            myPerson.AskQuestion(int.Parse(input));
-            Console.WriteLine("You are: " + myPerson.GetPersonality());
+            // Constructor
+            public ProgramMain(string name, int age, string grade) : base(name, age, grade)
+            {
             }
-            catch
+
+            static void Main(string[] args)
             {
-                Console.WriteLine("Please type numbers 1 or 2");
+                // Create a List of Student objects
+                List<Student> students = new List<Student>();
+
+                // Add new Student objects to the List
+                students.Add(new Student("Ivan Ivanovic", 18, "5"));
+                students.Add(new Student("Marko Markovic", 19, "3"));
+                students.Add(new Student("Lora Loric", 17, "4"));
+
+                // Use a foreach loop to access and display the properties of each Student object in the List
+                foreach (Student student in students)
+                {
+                    Console.WriteLine("------------STUDENT---------------");
+                    Console.WriteLine("Student Name: " + student.Name);
+                    Console.WriteLine("Student Age: " + student.Age);
+                    Console.WriteLine("Student Grade: " + student.Grade + "\n");
+                }
+
+                Console.ReadLine();
             }
         }
     }
 }
+
--- Student.cs
+++ Student.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace OOP_App
 {
-    //Here we can only read or write data to this class
-    internal class Student : Person
+    class Student
     {
-        
+        private string name;
+        private int age;
+        private string grade;
 
+        public Student(string name, int age, string grade)
+        {
+            Name = name;
+            Age = age;
+            Grade = grade;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public string Grade
+        {
+            get { return grade; }
+            set { grade = value; }
+        }
     }
 }
+
